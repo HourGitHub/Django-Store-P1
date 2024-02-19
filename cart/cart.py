0409aa8dad819fb3b675d4d4c930c82b91135de2
Keanghour Pho <keanghour.pho@canadiabank.com.kh>
2024-02-19 17:08:06 +0700

    Part2-3:10 error Cart-Summary

--- cart/cart.py
+++ cart/cart.py
@@ -12,7 +12,6 @@ class Cart:
         self.session = request.session
         cart = self.session.get("skey")
         if "skey" not in request.session:
-            # if not cart:
             cart = self.session["skey"] = {}
         self.cart = cart
 
@@ -24,12 +23,58 @@ class Cart:
         if product_id not in self.cart:
             self.cart[product_id] = {
                 "price": str(product.price), 
-                "qty": int(qty)}
+                "qty": int(qty)
+            }
+        else:
+            self.cart[product_id]['qty'] += int(qty)
 
         self.session.modified = True
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database and return products
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     def __len__(self):
         """"
         Get the cart data and count the qty of items
         """
         return sum(item['qty'] for item in self.cart.values())
+    
+
+    # def update(self, product, qty):
+    #     """
+    #     Update values in session data
+    #     """
+    #     product_id = str(product)
+    #     if product_id in self.cart:
+    #         self.cart[product_id]['qty'] = qty
+    #     self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+
+    # def delete(self, product):
+    #     """
+    #     Delete item from session data
+    #     """
+    #     product_id = str(product)
+
+    #     if product_id in self.cart:
+    #         del self.cart[product_id]
+    #         print(product_id)
+    #         self.save()
+
+    # def save(self):
+    #     self.session.modified = True
